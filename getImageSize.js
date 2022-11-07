f7ae6509bf3288ef4deced0f098ef2ba1115c182
Danny Alexander Mosquera <78458322+danny1099@users.noreply.github.com>
2022-11-07 11:15:13 -0500

    ajuste de comics con dimensiones de imagen

---
+++ getImageSize.js
@@ -0,0 +1,17 @@
+import http from 'https';
+import sizeOf from 'image-size';
+
+export const getImageDimensions = ({ url }) => {
+  return new Promise((resolve) => {
+    http.get(url, (response) => {
+      const chunks = [];
+      response
+        .on('data', (chunk) => chunks.push(chunk))
+        .on('end', () => {
+          const buffer = Buffer.concat(chunks);
+          const { height, width } = sizeOf(buffer);
+          resolve({ height, width });
+        });
+    });
+  });
+};
--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 import axios from 'axios';
 import fs from 'fs-extra';
+import { getImageDimensions } from './getImageSize.js';
 
 const MIN_ID_KCD = 2500;
 const MAX_ID_KCD = 2588;
 
 /* NOTE: Recorre en bucle los datos numericos de las variables y genera una llamada por cada numero */
-for (let i = MIN_ID_KCD; i < MAX_ID_KCD; i++) {
-  const URL = `https://xkcd.com/${i}/info.0.json`;
+for (let id = MIN_ID_KCD; id < MAX_ID_KCD; id++) {
+  const URL = `https://xkcd.com/${id}/info.0.json`;
 
   const { data } = await axios.get(URL);
+  const { num, news, transcript, img, ...restOfComic } = data;
+  const { height, width } = await getImageDimensions({ url: img });
+  const comicToStore = { id, img, height, width, ...restOfComic };
 
-  fs.writeJSON(`./comics/${i}.json`, data, { spaces: 2 });
+  fs.writeJSON(`./comics/${id}.json`, comicToStore, { spaces: 2 });
 }
